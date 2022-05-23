fae64141f32e0045eb5418762ffbd2eda0239a94
TuanAnh <tuananh1905>
2022-05-23 22:44:04 +0700

    slot 5

---
+++ slot5_staticConstructor_staticClass/slot5_staticConstructor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace slot5_staticConstructor_staticClass
+{
+    class MyClass
+    {
+        public static int x = 1;
+        static MyClass()
+        {
+            x = 2;
+            Console.WriteLine("Static constructor: x = {0}", x);
+        }
+        public MyClass()
+        {
+            x++;
+            Console.WriteLine("Object contructor: x = {0}", x);
+        }
+    }
+    static class UtilityClass
+    {
+        public static void PrintTime()
+        {
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass m1 = new MyClass();
+            Console.WriteLine();
+            MyClass m2 = new MyClass();
+            Console.WriteLine();
+            UtilityClass.PrintTime();
+        }
+    }
+}
